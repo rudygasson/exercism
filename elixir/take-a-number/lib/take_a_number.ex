cbf9988f6a4c688420527f311f0538fc55cdedec
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-10-25 20:31:54 +0200

    Implement counter agent

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,18 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(state) do
+    receive do
+      {:report_state, sender_id} ->
+        send(sender_id, state)
+        loop(state)
+      {:take_a_number, sender_id} ->
+        send(sender_id, state = state + 1)
+        loop(state)
+      :stop -> nil
+      _ -> loop(state)
+    end
   end
 end
