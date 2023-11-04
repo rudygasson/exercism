6a7cea7af2f8d3a6092175b0b5100db62247d69e
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-11-04 16:04:40 +0100

    Implement all solutions

--- elixir/paint-by-number/lib/paint_by_number.ex
+++ elixir/paint-by-number/lib/paint_by_number.ex
@@ -5,26 +5,33 @@ defmodule PaintByNumber do
   end
 
   def empty_picture() do
-    # Please implement the empty_picture/0 function
+    <<>>
   end
 
   def test_picture() do
-    # Please implement the test_picture/0 function
+    <<0::2, 1::2, 2::2, 3::2>>
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    color_bits = palette_bit_size(color_count)
+    <<pixel_color_index::size(color_bits), picture::bitstring>>
   end
 
+  def get_first_pixel(picture, _color_count) when picture == <<>>, do: nil
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    color_bits = palette_bit_size(color_count)
+    <<head::size(color_bits), _tail::bitstring>> = picture
+    head
   end
 
+  def drop_first_pixel(picture, color_count) when picture ==<<>>, do: picture
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    color_bits = palette_bit_size(color_count)
+    <<_head::size(color_bits), tail::bitstring>> = picture
+    tail
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
   end
 end
