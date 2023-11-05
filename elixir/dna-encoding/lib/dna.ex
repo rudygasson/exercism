55422424ba4ae0126a8303e8d1ce8f96b872426e
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-11-05 01:13:55 +0100

    iteration 1 for dna encoding

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,37 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0
+      ?A -> 1
+      ?C -> 2
+      ?G -> 4
+      ?T -> 8
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0 -> ?\s
+      1 -> ?A
+      2 -> ?C
+      4 -> ?G
+      8 -> ?T
+    end
   end
 
-  def encode(dna) do
-    # Please implement the encode/1 function
+  def encode(dna), do: do_encode(dna, <<>>)
+
+  defp do_encode([last], enc_dna), do: <<enc_dna::bitstring, encode_nucleotide(last)::4>>
+  defp do_encode([head | tail], enc_dna) do
+    acc = <<enc_dna::bitstring, encode_nucleotide(head)::4>>
+    do_encode(tail, acc)
   end
 
-  def decode(dna) do
-    # Please implement the decode/1 function
+  def decode(dna), do: do_decode(dna, [])
+
+  defp do_decode(<<last::4>>, dec_dna), do: dec_dna ++ [decode_nucleotide(last)]
+  defp do_decode(<<head::4, tail::bitstring>>, dec_dna) do
+    dec_dna = dec_dna ++ [decode_nucleotide(head)]
+    do_decode(tail, dec_dna)
   end
 end
