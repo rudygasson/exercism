2b48335c93fa408ddd0802847380a0c92115c89c
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-11-05 01:33:57 +0100

    Refactor tail recursion base cases

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -21,17 +21,15 @@ defmodule DNA do
 
   def encode(dna), do: do_encode(dna, <<>>)
 
-  defp do_encode([last], enc_dna), do: <<enc_dna::bitstring, encode_nucleotide(last)::4>>
+  defp do_encode([], enc_dna), do: enc_dna
   defp do_encode([head | tail], enc_dna) do
-    acc = <<enc_dna::bitstring, encode_nucleotide(head)::4>>
-    do_encode(tail, acc)
+    do_encode(tail, <<enc_dna::bitstring, encode_nucleotide(head)::4>>)
   end
 
   def decode(dna), do: do_decode(dna, [])
 
-  defp do_decode(<<last::4>>, dec_dna), do: dec_dna ++ [decode_nucleotide(last)]
+  defp do_decode(<<>>, dec_dna), do: dec_dna
   defp do_decode(<<head::4, tail::bitstring>>, dec_dna) do
-    dec_dna = dec_dna ++ [decode_nucleotide(head)]
-    do_decode(tail, dec_dna)
+    do_decode(tail, dec_dna ++ [decode_nucleotide(head)])
   end
 end
