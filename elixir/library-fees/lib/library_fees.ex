b6ef5b1cf54515eef3c95dd78d207c4549a39207
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-11-09 13:17:14 +0100

    First 2 methods, first try on third

--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -1,14 +1,21 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    {_, date_time} = NaiveDateTime.from_iso8601(string)
+    date_time
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    Time.before?(NaiveDateTime.to_time(datetime), ~T[12:00:00])
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    checkout_datetime
+    |> before_noon?
+    |> case do
+        true -> 28
+        false -> 29
+      end
+    |> NaiveDateTime.add(checkout_datetime, &1, :days)
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
