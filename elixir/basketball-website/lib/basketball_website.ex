19a36fc38d98f61ad33c9bbf45ae85a27f1e71f3
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-11-23 20:01:05 +0100

    Second solution for basketball_website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,11 +1,11 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    keys = String.split(path, ".")
-    if tl(keys) == [] do
-      data[hd(keys)]
-    else
-      extract_from_path(data[hd(keys)], Enum.join(tl(keys), "."))
-    end
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  def do_extract_from_path(data, []), do: data
+  def do_extract_from_path(data, [h | t]) do
+    do_extract_from_path(data[h], t)
   end
 
   def get_in_path(data, path) do
