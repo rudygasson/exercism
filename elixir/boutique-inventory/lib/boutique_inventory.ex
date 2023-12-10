ef06ec4ec520a2417634d5f740d86a32bc2f8ddc
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-12-10 18:05:26 +0100

    Refactor solutions

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,26 +1,29 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    Enum.sort_by(inventory, &(&1.price), :asc)
+    Enum.sort_by(inventory, & &1.price)
   end
 
   def with_missing_price(inventory) do
-    Enum.filter(inventory, &(&1.price == nil))
+    Enum.filter(inventory, &(!&1.price))
   end
 
   def update_names(inventory, old_word, new_word) do
     Enum.map(inventory, fn item ->
-      Map.update(item, :name, "", fn name ->
-        String.replace(name, old_word, new_word)
-      end)
+      new_name = String.replace(item.name, old_word, new_word)
+      %{item | name: new_name}
     end)
   end
 
   def increase_quantity(item, count) do
-    increase = fn {k,v} -> {k, v + count} end
-    Map.update(item, :quantity_by_size, 0, &(Map.new(&1, increase)))
+    new_quantities =
+      Map.new(
+        item.quantity_by_size,
+        fn {size, quantity} -> {size, quantity + count} end
+      )
+    %{item | quantity_by_size: new_quantities}
   end
 
   def total_quantity(item) do
-    Enum.reduce(item.quantity_by_size, 0, fn {_k, v}, acc -> acc + v end)
+    Enum.reduce(item.quantity_by_size, 0, fn {_size, quantity}, sum -> sum + quantity end)
   end
 end
