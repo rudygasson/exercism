795a1c4d9dd208b88cee3dee7bd0c207dc1e4fe6
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-12-12 15:23:28 +0100

    Solve task 1 and 2, first draft

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,27 @@
 defmodule FileSniffer do
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    case extension do
+      "exe" -> "application/octet-stream"
+      "bmp" -> "image/bmp"
+      "png" -> "image/png"
+      "jpg" -> "image/jpg"
+      "gif" -> "image/gif"
+      _ -> nil
+    end
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    case file_binary do
+      <<0x7F, _::binary-size(3), _::binary>> -> "application/octet-stream"
+      <<0x42, _::binary-size(1), _::binary>> -> "image/bmp"
+      <<0x89, _::binary-size(7), _::binary>> -> "image/png"
+      <<0xFF, _::binary-size(2), _::binary>> -> "image/jpg"
+      <<0x47, _::binary-size(2), _::binary>> -> "image/gif"
+      _ -> nil
+    end
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    
   end
 end
