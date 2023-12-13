e6ec8136d85d422445dbfa1759e755fc69a768ca
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-12-13 22:28:49 +0100

    Second draft for different approach

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,31 +1,47 @@
 defmodule FileSniffer do
+  @file_types [
+    {"exe", "application/octet-stream", <<0x7F, 0x45, 0x4C, 0x46>>},
+    {"bmp", "image/bmp", <<0x42, 0x4D>>},
+    {"png", "image/png", <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>},
+    {"jpg", "image/jpg", <<0xFF, 0xD8, 0xFF>>},
+    {"gif", "image/gif", <<0x47, 0x49, 0x46>>}
+  ]
+
   def type_from_extension(extension) do
-    case extension do
-      "exe" -> "application/octet-stream"
-      "bmp" -> "image/bmp"
-      "png" -> "image/png"
-      "jpg" -> "image/jpg"
-      "gif" -> "image/gif"
+    case Enum.find(@file_types, fn {ext, _, _} -> ext == extension end) do
+      {_, media_type, _} -> media_type
       _ -> nil
     end
   end
 
   def type_from_binary(file_binary) do
-    case file_binary do
-      <<0x7F, _::binary-size(3), _::binary>> -> "application/octet-stream"
-      <<0x42, _::binary-size(1), _::binary>> -> "image/bmp"
-      <<0x89, _::binary-size(7), _::binary>> -> "image/png"
-      <<0xFF, _::binary-size(2), _::binary>> -> "image/jpg"
-      <<0x47, _::binary-size(2), _::binary>> -> "image/gif"
-      _ -> nil
+    if is_binary(file_binary) do
+      case Enum.find(@file_types, fn {_, _, signature} ->
+             if byte_size(signature) <= byte_size(file_binary) do
+               <<sig::binary-size(byte_size(signature)), _::binary>> = file_binary
+               sig == signature
+             end
+           end) do
+        {_, media_type, _} -> media_type
+        _ -> nil
+      end
+    else
+      nil
     end
   end
 
   def verify(file_binary, extension) do
     type = type_from_extension(extension)
+
     cond do
       type && type == type_from_binary(file_binary) -> {:ok, type}
       true -> {:error, "Warning, file format and file extension do not match."}
+    end
+  end
+
+  defp get_media_type(param) do
+    case param do
+      _ -> nil
     end
   end
 end
