b79777f2ab797006c6d90a06709300660115b131
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-12-14 03:42:11 +0100

    Third approach, first draft

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,29 +1,36 @@
 defmodule FileSniffer do
-  @file_types [
-    {"exe", "application/octet-stream", <<0x7F, 0x45, 0x4C, 0x46>>},
-    {"bmp", "image/bmp", <<0x42, 0x4D>>},
-    {"png", "image/png", <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>},
-    {"jpg", "image/jpg", <<0xFF, 0xD8, 0xFF>>},
-    {"gif", "image/gif", <<0x47, 0x49, 0x46>>}
-  ]
+  @file_types %{
+    exe: "application/octet-stream",
+    bmp: "image/bmp",
+    png: "image/png",
+    jpg: "image/jpg",
+    gif: "image/gif"
+  }
+
+  @signatures %{
+    <<0x7F, 0x45, 0x4C, 0x46>> => :exe,
+    <<0x42, 0x4D>> => :bmp,
+    <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>> => :png,
+    <<0xFF, 0xD8, 0xFF>> => :jpg,
+    <<0x47, 0x49, 0x46>> => :gif
+  }
+
   @mismatch_message "Warning, file format and file extension do not match."
 
   def type_from_extension(extension) do
-    Enum.into(@file_types, %{}, fn {ext, type, _} -> {ext, type} end)[extension]
+    @file_types[String.to_atom(extension)]
   end
 
   def type_from_binary(file_binary) do
-    case Enum.find(@file_types, &check_signature(file_binary, elem(&1, 2))) do
-      {_, type, _} -> type
-      _ -> nil
-    end
+    sig = Map.keys(@signatures)
+    |> Enum.find(&(check_signature(file_binary, &1)))
+    @file_types[@signatures[sig]]
   end
 
   def verify(file_binary, extension) do
-    type = type_from_extension(extension)
-    cond do
-      type && type == type_from_binary(file_binary) -> {:ok, type}
-      true -> {:error, @mismatch_message}
+    case {type_from_extension(extension), type_from_binary(file_binary)} do
+      {type, type} when type != nil -> {:ok, type}
+      _ -> {:error, @mismatch_message}
     end
   end
 
