cc950127ac729d7bb08f8ac19353759e37874d48
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-12-14 02:10:40 +0100

    Refactor all final

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -6,14 +6,14 @@ defmodule FileSniffer do
     {"jpg", "image/jpg", <<0xFF, 0xD8, 0xFF>>},
     {"gif", "image/gif", <<0x47, 0x49, 0x46>>}
   ]
+  @mismatch_message "Warning, file format and file extension do not match."
 
   def type_from_extension(extension) do
-    Enum.into(@file_types, %{}, &extension_map/1)[extension]
+    Enum.into(@file_types, %{}, fn {ext, type, _} -> {ext, type} end)[extension]
   end
 
   def type_from_binary(file_binary) do
-    Enum.find(@file_types, &check_signature(file_binary, elem(&1, 2)))
-    |> case do
+    case Enum.find(@file_types, &check_signature(file_binary, elem(&1, 2))) do
       {_, type, _} -> type
       _ -> nil
     end
@@ -21,15 +21,11 @@ defmodule FileSniffer do
 
   def verify(file_binary, extension) do
     type = type_from_extension(extension)
-
     cond do
       type && type == type_from_binary(file_binary) -> {:ok, type}
-      true -> {:error, "Warning, file format and file extension do not match."}
+      true -> {:error, @mismatch_message}
     end
   end
-
-  defp extension_map({extension, media_type, _}), do: {extension, media_type}
-  defp type({_, media_type, _}), do: media_type
 
   defp check_signature(file_binary, signature) do
     case file_binary do
@@ -37,12 +33,4 @@ defmodule FileSniffer do
       _ -> false
     end
   end
-
-  defp get_media_type(signature) do
-    @file_types
-    |> Map.new(fn {_, media_type, signature} -> {signature, media_type} end)
-    |> Map.get(signature)
-  end
-
-
 end
